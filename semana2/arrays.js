f9a114d7688feae2dc2bc6adc0087b6604867f8f
Camilo Villalobos <villalobosandres989@gmail.com>
2026-05-11 14:49:46 -0500

    Arreglo

--- semana2/arrays.js
+++ semana2/arrays.js
@@ -10,13 +10,6 @@ let products = {
     }
 };
 
-
-
-// for(let key in products){
-//     console.log(key + ': ' + JSON.stringify(products[key]));
-// }
-
-
 const Mapa = new Map();
 Mapa.set('id', 'a1b2c3');
 Mapa.set('name', 'Laptop');
@@ -24,20 +17,23 @@ Mapa.set('name', 'Laptop');
 console.log(Mapa.get('name'));
 
 
+const numbers = [10, 20, 30,30, 40, 50, 51];
+const names = new Set(numbers);
+
+names.add(60);
+names.delete(20);
+
+for(let name of names){
+    console.log('-', name);
+}
+
 // for(let value of Object.values(products)){
 //     console.log(Object.keys(products)[Object.values(products).indexOf(value)] + ': ' + value);
 // }
 
 
-// const numbers = [10, 20, 30,30, 40, 50, 51];
-// const names = new Set(numbers);
 
-// names.add(60);
-// names.delete(20);
 
-// for(let name of names){
-//     console.log('-', name);
-// }
 
 
 // products.available = true;
@@ -58,33 +54,3 @@ console.log(Mapa.get('name'));
 //     }
 // }
 // person.saludar();
-
-
-// const estudiantes = [
-//     {
-//         name: 'Camilo',
-//         lastName: 'Villalobos',
-//         age: 25,
-//         courses: ['Math', 'Physics']
-//     },
-//     {
-//         name: 'Maria',
-//         lastName: 'Gomez',
-//         age: 22,
-//         courses: ['Literature', 'History']
-//     },
-//     {
-//         name: 'Juan',
-//         lastName: 'Perez',
-//         age: 28,
-//         courses: ['Biology', 'Chemistry']
-//     }
-// ]
-
-
-// for(let i = 0; i < estudiantes.length; i++){
-//     console.log('Name: ' + estudiantes[i].name);
-//     console.log('Last Name: ' + estudiantes[i].lastName);
-//     console.log('Age: ' + estudiantes[i].age);
-//     console.log('Courses: ' + estudiantes[i].courses);
-// }
